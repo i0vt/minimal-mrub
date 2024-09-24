e26dfaf0c5f1f8f4b58b05cc7a494e207e065069
tak <nerd@linux.pl>
2024-09-24 11:10:23 +0200

    start

---
+++ Makefile
@@ -0,0 +1,19 @@
+CPP      = g++
+CC       = gcc
+MM       = ./mruby
+MBU      = $(MM)/build/host
+OPCJE99  = -Wall -O0 -std=c99 -pedantic -pedantic-errors -static
+PROG     = przyk
+
+./$(PROG): $(PROG).c Makefile
+	$(CC) $(OPCJE99) -g -I $(MM)/include/ -I$(MM)/include/mruby $< \
+	  -o $(PROG) $(MBU)/lib/libmruby.a -lm
+	./$(PROG)
+
+mem: ./$(PROG)
+	valgrind --leak-check=full --show-reachable=yes -s --track-origins=yes ./$(PROG)
+
+.PHONY: clean
+
+clean:
+	rm -f ./$(PROG)
---
+++ przyk.c
@@ -0,0 +1,72 @@
+#include <stdlib.h>
+#include <mruby.h>
+#include <mruby/data.h>
+#include <mruby/class.h>
+#include <mruby/compile.h>
+
+#define SPOKO_NAZWA "Spoko"
+struct Spoko_Dane
+{
+ uint32_t jeden[16];
+ uint32_t dwa[16];
+};
+
+static const mrb_data_type Spoko_Typ =
+{
+ SPOKO_NAZWA, mrb_free
+};
+
+mrb_value Spoko_initialize(mrb_state* state, mrb_value self)
+{
+mrb_int n;
+
+ mrb_get_args(state, "i", &n);
+ struct Spoko_Dane *spo = (struct Spoko_Dane *)DATA_PTR(self);
+ if(spo){ mrb_free(state, spo); }
+
+ spo = (struct Spoko_Dane *)malloc(sizeof(struct Spoko_Dane));
+
+ for(uint32_t i = 0; i < 16; ++i)
+  {
+  spo->jeden[i] = i * n;
+  spo->dwa[i] = i * n * n;
+  }
+
+ mrb_data_init(self, spo, &Spoko_Typ);
+
+return self;
+}
+
+mrb_value Spoko_pisz(mrb_state* state, mrb_value self)
+{
+struct Spoko_Dane *spo;
+
+ Data_Get_Struct(state, self, &Spoko_Typ, spo);
+ mrb_assert(spo != NULL);
+ for(uint32_t i = 0; i < 16; ++i)
+  { printf("%d ",(uint32_t)spo->jeden[i]); }
+ printf(" -- ");
+ for(uint32_t i = 0; i < 16; ++i)
+  { printf("%d:", (uint32_t)spo->dwa[i]); }
+ printf("\n");
+
+return mrb_nil_value();
+}
+
+int main()
+{
+mrb_state *mrb = mrb_open();
+ if (!mrb) { printf("mruby nie działa\n"); return 1; }
+ printf(" Wczytałem mruby\n");
+
+ struct RClass *Spo = mrb_define_class(mrb, SPOKO_NAZWA, mrb->object_class);
+ MRB_SET_INSTANCE_TT(Spo, MRB_TT_DATA);
+ mrb_define_method(mrb, Spo, "initialize", Spoko_initialize, MRB_ARGS_REQ(1));
+ mrb_define_method(mrb, Spo, "pisz", Spoko_pisz, MRB_ARGS_NONE());
+
+ mrb_load_string(mrb, "a = Spoko.new(10); a.pisz; p a");
+
+ mrb_close(mrb);
+
+return 0;
+}
